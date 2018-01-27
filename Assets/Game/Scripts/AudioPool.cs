3c7e732333e1594fd22086adf241cd22b9023036
Ricardo Bustamante <ricardo.bustamante@tapps.com.br>
2018-01-27 16:08:28 -0200

    AudioPool

---
+++ Assets/Game/Scripts/AudioPool.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPool : MonoBehaviour
+{
+    private List<AudioSource> sources;
+
+    private AudioSource Source
+    {
+        get
+        {
+            foreach (var source in sources)
+            {
+                if (!source.isPlaying)
+                {
+                    return source;
+                }
+            }
+            return gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public void PlayAudio(AudioClip clip, float pitch = 1, float volume = 1)
+    {
+        var src = Source;
+        src.volume = volume;
+        src.pitch = pitch;
+        src.clip = clip;
+        src.Play();
+    }
+}
